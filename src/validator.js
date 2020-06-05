623ecd7b79ba14c28e616c40f3e660f39e46bcc3
Camila Yanez <ccynzz@gmail.com>
2020-06-05 11:39:35 -0500

    primera version validacion

--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,21 @@
 const validator = {
-  // ...
+  isvalid: function(creditCardNumber){
+    
+    //return =
+  }
+
 };
+//Obtener el numero de la tdc ingresado en el input y almacenar en una variable
+let creditCardNumber = document.getElementById('card');
+//Obtener el inverso del numero de la tarjeta
+// function invert(creditCardNumber){
+//   let i = creditCardNumber.length;
+//   let creditCarNumberInvest = "";
+//   while (i>=0){
+//     creditCarNumberInvest = creditCarNumberInvest + creditCardNumber.charAt(i);
+//     i--;
+//   }
+//   return creditCarNumberInvest;
+// }
 
 export default validator;
