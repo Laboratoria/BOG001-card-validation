5ad1d6c649a3f8b428debd811ea8596531c83a1c
Camila Yanez <ccynzz@gmail.com>
2020-06-12 10:16:43 -0500

    consolidacion de archivos

--- src/validator.js
+++ src/validator.js
@@ -5,20 +5,21 @@ const validator = {
   isvalid: function(cardNumber){
     //Convertir el numero de la tdc en array y obtener el inverso
     let algorithmArray = cardNumber.split('').reverse();
-    
+      
 //----------------------------- forEach ----------------------------------------------
     algorithmArray.forEach((element, index) => {
 
     //Obtener numeros en las posiciones par y multiplicarlos por 2
      if (index & 1) {
       element = element * 2
-
+      console.log(element);
       //A numeros mayores o iguales a 10, convertirlos en string y separar los dos digitos con split 
       if (element >= 10) {
         element = element.toString().split('');
 
         //Convertir los digitos en numeros y sumarlos entre si
         element = parseInt(element[0]) + parseInt(element[1]);
+        console.log(element)
       }
     } 
 
