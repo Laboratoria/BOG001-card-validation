0e4e0bb7a1b670cc1b21a2a87751aece2aa46688
Camila Yanez <ccynzz@gmail.com>
2020-06-14 09:14:31 -0500

    metodos isValid y maskify en validator.js + interaccion de estos metodos con el DOM por index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,44 @@
 import validator from './validator.js';
-console.log(validator.isvalid);
 
+console.log(validator);
+
+const inputNumberTC = document.getElementById('inputNumberCard');
+const btnSubmit = document.getElementById('validDataCard');
+// Validación tarjeta
+const errorValidation = document.getElementById('errorValidation')
+
+inputNumberTC.addEventListener('blur', function(){
+  const isValid = validator.isValid(inputNumberTC.value);
+  console.log(isValid);
+  
+  if (isValid == false) {
+    errorValidation.textContent = "El numero de tarjeta ingresado es invalido"
+  } else {
+    errorValidation.textContent = ""
+  }
+  
+  let mask = validator.maskify(inputNumberTC.value);
+  inputNumberTC.value = mask;  
+  
+});
+
+// Reflejar Número en TC, no admitir letras en el campo
+const numberCreditCard = document.getElementById('dataNumber');
+
+inputNumberTC.addEventListener('keyup', e => {
+  let numberValue = e.target.value;//Guardar el valor del input para abajo generar método replace con expresiones regulares
+  inputNumberTC.value = numberValue.replace(/\D/g, '');// \D = Busca las letras y las reemplaza por string vacío
+  numberCreditCard.textContent = numberValue;
+  if(numberValue == ''){
+    numberCreditCard.textContent = '#### #### #### ####';
+  }
+});
+
+// Cambio de pantallas
+const buttonBuy = document.getElementById('btnBuy'); //Botón comprar
 const instrumentsPage = document.getElementById('instruments'); //pantalla 1
 const paymentPage = document.getElementById('payment'); //pantalla 2
-const buttonBuy = document.getElementById('btnBuy'); //Botón comprar
-const buttonDisabled = document.querySelector('.btnDisabled'); //Botón agotado, solo selecciona el primer elemento de la clase
-const form = document.getElementById('formCard'); //Formulario
-const numberCreditCard = document.getElementById('dataNumber');
-const inputNumberTC = document.getElementById('inputNumberCard');
-const nameCreditCard = document.getElementById('dataName');
-const inputNameTC = document.getElementById('inputName');
-const monthCreditCard = document.getElementById('month');
-const inputMonthTC = document.getElementById('selectMonth');
-const yearCreditCard = document.getElementById('year');
-const inputYearTC = document.getElementById('selectYear');
 
-//Cambio de pantallas
 buttonBuy.addEventListener('click', showPayment);
 
 function showPayment() {
@@ -23,14 +46,16 @@ function showPayment() {
   instrumentsPage.classList.add('hidden');
 }
 
-//Alerta de no stock para botón "Agotado"
+// Alerta de no stock para botón "Agotado"
+const buttonDisabled = document.querySelector('.btnDisabled'); //Botón agotado, solo selecciona el primer elemento de la clase
 buttonDisabled.addEventListener('click', noStock);
 
 function noStock() {
   alert('Muy pronto lo tendremos en stock');
 }
 
-//Menú desplegable en el select mes
+const form = document.getElementById('formCard'); //Formulario
+// Menú desplegable en el select mes
 for (let i = 1; i <= 12; i++) {
   let optionMonths = document.createElement('option');
   optionMonths.value = i;
@@ -47,17 +72,10 @@ for(let i = yearActual; i <= yearActual + 4; i++){
 	form.selectYear.appendChild(optionYears);
 }
 
-//Reflejar Número en TC, no admitir letras en el campo
-inputNumberTC.addEventListener('keyup', e => {
-  let numberValue = e.target.value;//Guardar el valor del input para abajo generar método replace con expresiones regulares
-	inputNumberTC.value = numberValue.replace(/\D/g, '');// \D = Busca las letras y las reemplaza por string vacío
-	numberCreditCard.textContent = numberValue;
-	if(numberValue == ''){
-		numberCreditCard.textContent = '#### #### #### ####';
-  }
-});
+// Reflejar Nombre en TC 
+const nameCreditCard = document.getElementById('dataName');
+const inputNameTC = document.getElementById('inputName');
 
-//Reflejar Nombre en TC 
 inputNameTC.addEventListener('keyup', e => {
   let nameValue = e.target.value;
   inputNameTC.value = nameValue.replace(/\d/g,'').replace(/\W/g,'');// \d= Dígitos \W= Ni letra, ni dígito
@@ -67,8 +85,12 @@ inputNameTC.addEventListener('keyup', e => {
   }
 });
 
-//Reflejar Mes en TC
+// Reflejar Mes en TC
+const monthCreditCard = document.getElementById('month');
+const inputMonthTC = document.getElementById('selectMonth');
 inputMonthTC.addEventListener('change', e => monthCreditCard.textContent = e.target.value);
 
-//Reflejar año en TC
+// Reflejar año en TC
+const inputYearTC = document.getElementById('selectYear');
+const yearCreditCard = document.getElementById('year');
 inputYearTC.addEventListener('change', e => yearCreditCard.textContent = e.target.value.slice(2));
