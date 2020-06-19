8dfd1a7369920c0e878cfa9cef5c2748d70aa77b
Angela Castro <kimypunchos@hotmail.com>
2020-06-18 19:22:17 -0500

    Cambios finales del proyecto

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import validator from './validator.js';
 // Llamamos a los atributos
-const tarjeta = document.getElementById('name');
 const numeroTarjeta = document.getElementById('creditCardNumber').value;
 document.getElementById('validarBoton').addEventListener("click", validar);
-console.log(tarjeta);
+//console.log(tarjeta);
 function validar(){
-   // console.log('hola'); lo usamos para probar el boton
-   validator.isValidCard(numeroTarjeta)
-};
-
-//invocamos a la función para validar la tarjeta de crédito que brinde el usuario
-//validator.isVadilCard()
+   //console.log('hola'); //lo usamos para probar el boton
+   validator.isValid(numeroTarjeta)
+   if(validator.isValid(numeroTarjeta) === true){
+      alert("Señor usuario su Tarjeta de Crédito es: Valida")
+   }else {
+      alert("Señor usuario su Tarjeta de crédito es: No Valida")
+   }
+}
 //console.log(numeroTarjeta);
 
 //console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,57 +1,48 @@
 const validator = {
-//creamos función para validar si una tarjeta de crédito es válida
-isValidCard:(creditCardNumber) => {
-  
-  //Condicionamos a que e usuario no deje el campo el blanco y que no ingrese dígitos que no sean números.
-  while (creditCardNumber === ''|| !creditCardNumber.match(creditCardNumber)) {
-    creditCardNumber = prompt('Escribe un número de tarjeta válido')
-  };
-
-  //Este array va a contener los números del cardNumber.
-  var arr = [];
-  //Recorre los números del string y los almacena en el array vacío.
-  for (var i = 0; i < creditCardNumber.length; i++) {
-    arr.push(parseInt(creditCardNumber[i]))
-  };
-
-  //ponemos los números de la tarjeta que estan en un array en orden inverso
-  var reverseCreditCardNumber = arr.reverse();
-
-  for (var j = 0; j < reverseCreditCardNumber.length; j++); {
-    //buscamos los números en posiciones impares del arreglo
-    //if(j%2!=0) --->otra forma de buscar impares
-    if (j % 2 === 1) {
-      //aquellos en posiciones impares los multiplicamos por 2
-      var duplicate = reverseCreditCardNumber[j] * 2;
-      //de cada uno de aquellos multiplicados cuyo producto sea de dos dígitos vamos a sumar sus dígitos y los vamos poniendo el array inverso en sus posiciones correspondientes
-      reverseCreditCardNumber[j] = parseInt(duplicate / 10) + duplicate % 10;
-    };
-  //creamos varianle sum donde vamos a sumar los elementos del array inverso
-  var sum = 0;{
-  //vamos a recorrer el array inverso para poder sumar sus elementos
-  for (var k = 0; k < reverseCreditCardNumber.length; k++) 
-    //sumamos cada uno de los elementos del array inverso
-    sum += reverseCreditCardNumber[k];
-  
-  //retornamos TARJETA VALIDA  si el residuo de 10 de la suma es igual a 0 y TARJETA INVALIDA si eso es falso.
-  return sum % 10 === 0 ? alert('tarjeta valida') : alert('tarjeta invalida');
-}
-  }
-
+  isValid:(creditCardNumber) => {
+    let valor = creditCardNumber.split("").reverse()
+    let guardar= 0;
+    let posicionArray = 1;
+    valor.forEach( numerito => { 
+      let pares  = (posicionArray % 2);
+      if (pares == 0) {
+        let resultadoParcial = (Number(numerito)*2);
+        if (resultadoParcial >= 10) {
+          let parcialValueString = resultadoParcial.toString();
+          let arrayNumeritos = parcialValueString.split("");
+          let resultadoCuenta = 0;
+          arrayNumeritos.forEach((numerito) => {
+          resultadoCuenta += Number(numerito);
+        });
+        guardar += resultadoCuenta;
+      
+      } else {
+          guardar += resultadoParcial;
+        }
+      } else {
+        guardar +=  Number(numerito);
+      }
+      posicionArray++;
+    })
+    return((guardar %10) == 0);
 },
+
 // Enmascaramos los numeros de la tarjeta
 maskify(creditCardNumber){
-//Ponemos la condición de que si tiene menos de 4 numeros nos retorne el mismo número
-    if(creditCardNumber.length < 4){
-      return creditCardNumber;
-    }
-// Si no, creamos variables que nos ayudan a enmascarar sin importar la mayor longitud los numeros
-    else {var cantidadNumeros = creditCardNumber.length; 
-      var lastDigit = creditCardNumber.slice(- 4);
-      var masked = "#".repeat(cantidadNumeros - 4); 
-      return masked+lastDigit
-    };
-},
-};
-    
-export default validator;
+  //Ponemos la condición de que si tiene menos de 4 numeros nos retorne el mismo número
+      if(creditCardNumber.length < 4){
+        return creditCardNumber;
+      }
+  // Si no, creamos variables que nos ayudan a enmascarar sin importar la mayor longitud los numeros
+      else {var cantidadNumeros = creditCardNumber.length; 
+        var lastDigit = creditCardNumber.slice(- 4);
+        var masked = "#".repeat(cantidadNumeros - 4); 
+        return masked+lastDigit
+      }
+  },
+  };
+
+
+  export default validator;
+
+  
