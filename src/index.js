cb108d04874a59f28c6aab196d28d27d024672f2
blarteagar <64844621+blarteagar@users.noreply.github.com>
2020-06-28 17:20:36 -0500

    archivos nuevos

--- src/index.js
+++ src/index.js
@@ -1,34 +1,56 @@
 import validator from './validator.js';
 
-const botonComprar1 = document.getElementById ("comprar1");
+const botonComprar1 = document.getElementById ('comprar1');
 botonComprar1.addEventListener("click",mostrar);
 
-const botonComprar2 = document.getElementById ("comprar2");
+const botonComprar2 = document.getElementById ('comprar2');
 botonComprar2.addEventListener("click",mostrar);
 
 //Vamos a ejecutar una función que se va a llamar mostrar y me va a capturar lo que tenemos como Mostrarform en el html, y me lo va a mostrar con block
 function mostrar(){
-    document.getElementById('catalog').style.display="none";
-    document.getElementById('captura').style.display="block";
-    }
-    const botonValidar = document.getElementById ("tarjeta");
-    botonValidar.addEventListener("click",varImportante);
+     document.getElementById('catalogo').classList.add('hidden');
+     document.getElementById('formulario').classList.remove('hidden');
     
-function varImportante () {
 
-      document.getElementById("captura").style.display="none";
-      document.getElementById("final").style.display="block";
-      let tc = document.getElementById ("tarjetacredito").value;
+    const botonValidar = document.getElementById ('tarjeta');
+    botonValidar.addEventListener("click",varImportante);
+}
+function varImportante(){
+
+     document.getElementById('formulario').classList.add('hidden');
+      let tc = document.getElementById ('tarjetacredito').value;
       let validation = validator.isValid(tc);
       let mask = validator.maskify(tc);
+      document.getElementById('resultado').classList.remove('hidden');
 
       if (validation === true){
-        document.getElementById("resultado").innerHTML = "Tu tarjeta de crédito No: " + mask + " es válida"; 
+      /* document.getElementById('resultado').classList.remove('hidden'); */
+        document.getElementById('resultado').innerHTML = "Tu tarjeta de crédito No: " + mask + " es válida";
+      /*alert ("Tu tarjeta de crédito No: " + mask + " es válida");*/
         } 
     
       else {
-        document.getElementById("resultado").innerHTML = "Tu tarjeta de crédito No: " + mask + " es inválida"; 
+      /*  document.getElementById('resultado').classList.remove('hidden'); */
+        document.getElementById('resultado').innerHTML = "Tu tarjeta de crédito No: " + mask + " NO es válida";
+        /*alert ("Tu tarjeta de crédito No: " + mask + " NO es válida");*/
         }
-      console.log(validation);
-      console.log(mask);
-    }
+      
+      
+      /* const botonContinuar=document.getElementById('siguiente');
+      botonContinuar.addEventListener("click",seguir); */
+
+      }
+
+
+     
+
+
+     /* const botonContinuar=document.getElementById('siguiente');
+     botonContinuar.addEventListener("click",seguir);
+     
+     }
+
+      function seguir(){
+        document.getElementById('catalogo').classList.remove('hidden');
+      }
+      */
